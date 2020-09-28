15473f6306aec754570c529b169dd16e878c963b
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-09-28 11:27:23 +0900

    3. 에어비엔비 디자인 하기 환경 설정 완료

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import './App.css';
 
 function App() {
-  const MyBox = styled.div`
-    display: grid;
-    color: red;
-  `;
-
-  return (
-    <MyBox>
-      <div>Hello jh</div>;
-    </MyBox>
-  );
+  return <div>Hello jh</div>;
 }
 
 export default App;
